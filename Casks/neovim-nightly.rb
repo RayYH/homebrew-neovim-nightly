8c3cf5671748b8a373960b4c8fa80cafba649700
RayYH <41055099+RayYH@users.noreply.github.com>
2026-01-06 05:19:10 +0000

    chore(cask): update neovim-nightly from template

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -2,8 +2,8 @@ cask "neovim-nightly" do
   version :latest
 
   arch arm: "arm64", intel: "x86_64"
-  sha256 arm:   "8a29be6ebedbaa592d3200854945d9391f6381a5e618a45d09d4ec60492d76fb",
-         intel: "20c0aad9fcda122d7e2f9657a058fbd222c7fd092cffb96d670c0dc2c7e5666f"
+  sha256 arm:   "4447685fd2c2345e8f652f297360d47ad8c8b72018df1f5e880769776fcc1a3e",
+         intel: "3ea320762eccdd5140d8e1043c080191e7096f61dffc95c35c0b512e26c15273"
 
   url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
       verified: "github.com/neovim"
